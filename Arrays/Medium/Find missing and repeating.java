194dc224be5ccc3b3fef2d53bd5c54ac58fe2eb5
Rahul Dwivedi <51382525+rahuldwvdi@users.noreply.github.com>
2022-03-21 02:51:10 +0530

    added cycle sort soln

--- Arrays/Medium/Find missing and repeating.java
+++ Arrays/Medium/Find missing and repeating.java
@@ -20,3 +20,31 @@ class Solve {
         return res;
     }
 }
+
+//using cycle sort, O(1) space
+class Solve {
+    int[] findTwoElement(int arr[], int n) {
+        int i = 0;
+        int[] res = new int[2];
+        while(i<n){
+            int correctindex = arr[i]-1;
+            if(arr[i]!=arr[correctindex]){
+                swap(arr,i,correctindex);
+            }
+            else    i++;
+        }
+        for(int j=0;j<n;j++){
+            if(arr[j]!=j+1){
+                res[0] = arr[j];
+                res[1] = j+1;
+            }
+        }
+        return res;
+    }
+    void swap(int arr[], int i, int ci){
+        int temp = arr[i];
+        arr[i] = arr[ci];
+        arr[ci] = temp;
+    }
+}
+
